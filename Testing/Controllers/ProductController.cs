559a9a53f26a3b339e6bc7456f45ca956b51ccec
Casey Hudler <chudler624@gmail.com>
2022-09-20 10:59:14 -0400

    project complete

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -58,5 +58,11 @@ namespace Testing.Controllers
             return RedirectToAction("Index");
         }
 
+        public IActionResult DeleteProduct(Product product)
+        {
+            repo.DeleteProduct(product);
+            return RedirectToAction("Index");
+        }
+
     }
 }
